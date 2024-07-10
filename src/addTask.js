295e42d478df691ad1b87129315d91b4a6f563cb
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-10 20:31:03 -0300

    create task

---
+++ src/addTask.js
@@ -0,0 +1,38 @@
+const { v4 } = require("uuid");
+const aws = require("aws-sdk");
+
+const addTask = async (data) => {
+  try {
+    const { title, description } = JSON.parse(data.body);
+
+    const createdAt = new Date();
+    const id = v4();
+    const dynamoDb = new aws.DynamoDB.DocumentClient();
+    const newTask = {
+      id,
+      title,
+      description,
+      createdAt,
+    };
+
+    await dynamoDb
+      .put({
+        TableName: "TaskTable",
+        Item: newTask,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newTask),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
+
+module.exports = { addTask };
